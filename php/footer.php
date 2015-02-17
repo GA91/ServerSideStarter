eb2d1c14e17a1aaca0527a490bc5be534f7cb832
Kingdom Creation Inc <kingdomcreation@gmail.com>
2015-02-17 12:00:42 -0500

    Initial commit

---
+++ php/footer.php
@@ -0,0 +1,12 @@
+<?php if( !defined('OK') ){ exit(); } 
+
+      if( isset($mysqli) ){ 
+          
+          $mysqli->close(); 
+      }
+?>
+    
+	<script type="text/javascript" src="js/main.js"></script>
+    
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,11 @@
+<?php if( !defined('OK') ){ exit(); } ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo APP_NAME ?></title>
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+    <script>var SCRIPT_NAME="<?php echo $filename; ?>"</script>
+</head>
+
+<body>
